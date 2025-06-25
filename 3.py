e61f29a649d11bc55ad6d9a89c63fff71498c517
henrytran58 <henry.ndk.tran@gmail.com>
2025-06-25 01:12:44 -0400

    day 3

--- 3.py
+++ 3.py
@@ -2,3 +2,11 @@
 #136. Single Number
 #https://leetcode.com/problems/single-number/
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+
+        for n in nums:
+            result ^= n
+
+        return result
