09f44ecfab6285e3f9dd80be254d96a6ca773277
henrytran58 <henry.ndk.tran@gmail.com>
2025-06-26 16:37:47 -0400

    day 5

--- 5_lc167.py
+++ 5_lc167.py
@@ -2,3 +2,23 @@
 #167. Two Sum II - Input Array is Sorted
 #https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
 
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        #Your solution must use only constant extra space.
+        #Brute force: O(1) space but O(n^2) time
+        #Target: O(1) space and O(1) time
+        #Q: Why is it given us the sorted array?
+        #A: 2 pointers
+
+        left = 0
+        right = len(numbers) - 1
+
+        while numbers[left] + numbers[right] != target:
+            if numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                right -= 1
+        
+        return [left + 1, right + 1]
+
+
