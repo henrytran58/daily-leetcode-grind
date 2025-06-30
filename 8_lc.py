d929ea3b8623d079a1671227c477b272a93c2693
henrytran58 <henry.ndk.tran@gmail.com>
2025-06-30 00:48:55 -0400

    day 8

--- 8_lc.py
+++ 8_lc.py
@@ -1,3 +1,22 @@
 #Sun, Jun 29, 2025
 #680. Valid Palindrome II
 #https://leetcode.com/problems/valid-palindrome-ii/description/
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if len(s) == 1 or len(s) == 0:
+            return True
+        elif s[0] == s[-1]:
+            return self.validPalindrome(s[1:-1])
+        else:
+            return self.validPalindromeDeleted(s[1:len(s)]) or self.validPalindromeDeleted(s[0:-1])
+    
+    
+
+    def validPalindromeDeleted(self, s):
+        if len(s) == 1 or len(s) == 0:
+            return True
+        elif s[0] != s[-1]:
+            return False
+        else:
+            return self.validPalindromeDeleted(s[1:-1])
