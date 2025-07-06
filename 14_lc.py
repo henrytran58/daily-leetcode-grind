ff7a3e4df0a53ae612dbed431ac61db770302ea2
henrytran58 <henry.ndk.tran@gmail.com>
2025-07-05 23:37:18 -0400

    day 14

--- 14_lc.py
+++ 14_lc.py
@@ -1,3 +1,14 @@
 #Sat, July 5, 2025
 #2114. Maximum Number of Words Found in Sentences
 #https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/description/
+
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        Max = 0
+
+        for sentence in sentences:
+            number_of_words = sentence.count(" ") 
+            if number_of_words + 1 > Max:
+                Max = number_of_words + 1
+        
+        return Max
