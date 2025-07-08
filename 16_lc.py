21d9c2fbc80fc3f9005fed05907c0a1a228f5e88
henrytran58 <henry.ndk.tran@gmail.com>
2025-07-08 00:27:58 -0400

    day 16

--- 16_lc.py
+++ 16_lc.py
@@ -2,3 +2,14 @@
 #520. Detech Capital
 #https://leetcode.com/problems/detect-capital/description/
 
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        all_cap = True if word[0].isupper() else False
+        all_lower = True
+        for ch in word[1:]:
+            if not ch.isupper():
+                all_cap = False
+            else:
+                all_lower = False
+        
+        return all_lower or all_cap
